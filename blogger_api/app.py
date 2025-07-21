da920800bc35d264d90883d25432ed0c4ebc38b4
Ar-jay Dayanan <acdayanan.bposeats@gmail.com>
2025-07-22 06:03:10 +0800

    Use flask_cors

--- blogger_api/app.py
+++ blogger_api/app.py
@@ -4,10 +4,12 @@ import sys
 from flask import Flask
 from flasgger import Swagger
 from flask_migrate import Migrate
-from src.db import db
-from src.blueprints import blogger_api
+from flask_cors import CORS
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
+
+from src.db import db
+from src.blueprints import blogger_api
 
 def create_app():
     app = Flask(__name__)
@@ -20,5 +22,6 @@ def create_app():
     app.register_blueprint(blogger_api, url_prefix="/blogger-api")
 
     Swagger(app)
+    CORS(app)
 
     return app
